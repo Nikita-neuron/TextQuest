493ec925d996c69a7d0fadb415617ad6e89e545c
Nikita-neuron <64783326+Nikita-neuron@users.noreply.github.com>
2020-12-08 20:27:09 +0300

    Some comments

--- app/src/main/java/com/example/textquest/MainActivity.java
+++ app/src/main/java/com/example/textquest/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Action[] actions = location.actions;
 
         textView.setText(location.text);
-//        Log.d("loc", location.text);
         for (int i = 0; i < actions.length; i++) {
             Button button = new Button(this);
             button.setText(actions[i].text);
